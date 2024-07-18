ee29f4c70f9ae71780fe01344ca5822ee8f4ad7b
Dunya-coder <diloromabdusattorova90@gmail.com>
2024-07-18 14:57:17 +0500

    Add

--- BasicGitHub/Program.cs
+++ BasicGitHub/Program.cs
@@ -10,6 +10,51 @@ namespace BasicGitHub
     {
         static void Main(string[] args)
         {
+            //1-Calculator
+            int firtnumber, secondnumber;
+            Console.WriteLine("first number=");
+            firtnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("operation:");
+              string operation;
+            operation = Console.ReadLine();
+            Console.WriteLine("second number=");
+
+            secondnumber = Convert.ToInt32(Console.ReadLine());
+            switch(operation)
+            {
+                case "+": Console.WriteLine($"{firtnumber}+{secondnumber}={firtnumber + secondnumber}"); break;
+                case "-":
+                    Console.WriteLine($"{firtnumber}-{secondnumber}={firtnumber - secondnumber}");break;
+                case "*":
+                    Console.WriteLine($"{firtnumber}*{secondnumber}={firtnumber * secondnumber}");break;
+                case "/":
+                    Console.WriteLine($"{firtnumber}/{secondnumber}={firtnumber / secondnumber}");break;
+                default:
+                    Console.WriteLine("Operation not found");break;
+            }
+            //2
+            Console.Write("N=");
+            int N,s=0;
+            again:
+            N = Convert.ToInt32(Console.ReadLine());
+            if (N > 0)
+            {
+                for (int i = 1; i < N; i++)
+                {
+                    s += i;
+                }
+                Console.WriteLine(s);
+            }
+            else
+                Console.WriteLine("ENTER POSITIVE NUMBER");
+            goto again;
+            //3
+            
+           int a = Convert.ToInt32(Console.ReadLine());
+            if(a%2==0)
+            { Console.WriteLine("number even"); }
+            else Console.WriteLine("number even");
+
         }
     }
 }
