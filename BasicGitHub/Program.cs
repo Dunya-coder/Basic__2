b32289fdff8d795630686c290c2f88fcf97d4c0b
Dunya-coder <diloromabdusattorova90@gmail.com>
2024-07-28 12:31:08 +0500

    code refactored

--- BasicGitHub/Program.cs
+++ BasicGitHub/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculator;
 
 namespace BasicGitHub
 {
@@ -12,48 +13,27 @@ namespace BasicGitHub
         {
             //1-Calculator
             int firtnumber, secondnumber;
-            Console.WriteLine("first number=");
+            Console.WriteLine("first number:");
             firtnumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("operation:");
-              string operation;
-            operation = Console.ReadLine();
-            Console.WriteLine("second number=");
 
-            secondnumber = Convert.ToInt32(Console.ReadLine());
-            switch(operation)
-            {
-                case "+": Console.WriteLine($"{firtnumber}+{secondnumber}={firtnumber + secondnumber}"); break;
-                case "-":
-                    Console.WriteLine($"{firtnumber}-{secondnumber}={firtnumber - secondnumber}");break;
-                case "*":
-                    Console.WriteLine($"{firtnumber}*{secondnumber}={firtnumber * secondnumber}");break;
-                case "/":
-                    Console.WriteLine($"{firtnumber}/{secondnumber}={firtnumber / secondnumber}");break;
-                default:
-                    Console.WriteLine("Operation not found");break;
-            }
-            //2
+            Console.WriteLine("operation:");
+          string  operation = Console.ReadLine();
+          
+            Console.WriteLine("second number:");
+           secondnumber = Convert.ToInt32(Console.ReadLine());
+
+           Calculator k = new Calculator();
+           k.calculate(firtnumber,secondnumber,operation);
+            //2 SUM OF NUMBERS FROM 1 TO N
             Console.Write("N=");
             int N,s=0;
             again:
             N = Convert.ToInt32(Console.ReadLine());
-            if (N > 0)
-            {
-                for (int i = 1; i < N; i++)
-                {
-                    s += i;
-                }
-                Console.WriteLine(s);
-            }
-            else
-                Console.WriteLine("ENTER POSITIVE NUMBER");
-            goto again;
-            //3
+           k.sum_ofnum(N);
+            //3//even number or odd number
             
            int a = Convert.ToInt32(Console.ReadLine());
-            if(a%2==0)
-            { Console.WriteLine("number even"); }
-            else Console.WriteLine("number even");
+            k.even_or_odd(a);
 
         }
     }
